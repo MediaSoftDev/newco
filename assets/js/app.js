f5b3d27855c4326e60c54e46dac7f4c4a0a80bc5
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-11-16 10:13:51 -0500

    Format code app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,78 +1,76 @@
-const menu = document.querySelector("#mobile-menu")
-const menuLinks = document.querySelector(".navbar__menu")
-const logo = document.querySelector(".navbar__logo")
-const body = document.querySelector("body")
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const logo = document.querySelector(".navbar__logo");
+const body = document.querySelector("body");
 
-const mobileMenu = ()=>{
-    menu.classList.toggle("is-active")
-    menuLinks.classList.toggle("active")
-    body.classList.toggle("active")
-}
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+  body.classList.toggle("active");
+};
 
-menu.addEventListener('click', mobileMenu)
+menu.addEventListener("click", mobileMenu);
 
 // Animations
 // GSAP 3
 // ScrollTrigger
- 
 
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.from(".animate-hero", {
+  duration: 0.6,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+});
 
-gsap.from('.animate-hero', {
-    duration: 0.6,
-    opacity: 0,
-    y: -150,
-    stagger: 0.3
-})
+gsap.from(".animate-services", {
+  scrollTrigger: ".animate-services",
+  duration: 0.5,
+  opacity: 1,
+  x: -150,
+  stagger: 0.12,
+});
 
-gsap.from('.animate-services', {
-    scrollTrigger: '.animate-services',
-    duration: 0.5,
-    opacity: 1,
-    x: -150,
-    stagger: 0.12
-})
+gsap.from(".animate-img", {
+  scrollTrigger: ".animate-services",
+  duration: 1.2,
+  opacity: 0,
+  x: -200,
+});
 
-gsap.from('.animate-img', {
-    scrollTrigger: '.animate-services',
-    duration: 1.2,
-    opacity: 0,
-    x: -200,
-})
+gsap.from(".animate-membership", {
+  scrollTrigger: ".animate-membership",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+  delay: 0.5,
+});
 
-gsap.from('.animate-membership', {
-    scrollTrigger: '.animate-membership',
-    duration: 1,
-    opacity: 0,
-    y: -150,
-    stagger: 0.3,
-    delay: 0.5
-})
+gsap.from(".animate-card", {
+  scrollTrigger: ".animate-card",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.1,
+  delay: 0.2,
+});
 
-gsap.from('.animate-card', {
-    scrollTrigger: '.animate-card',
-    duration: 1,
-    opacity: 0,
-    y: -150,
-    stagger: 0.1,
-    delay: 0.2
-})
+gsap.from(".animate-work", {
+  scrollTrigger: ".animate-work",
+  duration: 1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+  delay: 0.2,
+});
 
-gsap.from('.animate-work', {
-    scrollTrigger: '.animate-work',
-    duration: 1,
-    opacity: 0,
-    y: -150,
-    stagger: 0.3,
-    delay: 0.2
-})
-
-gsap.from('.animate-email', {
-    scrollTrigger: '.animate-email',
-    duration: 0.8,
-    opacity: 0,
-    y: -150,
-    stagger: 0.25,
-    delay: 0.6
-})
+gsap.from(".animate-email", {
+  scrollTrigger: ".animate-email",
+  duration: 0.8,
+  opacity: 0,
+  y: -150,
+  stagger: 0.25,
+  delay: 0.6,
+});
--- assets/js/funcs.js
+++ assets/js/funcs.js
@@ -1,27 +1,30 @@
-$(function(){
-    smmothScroll(800)
-})
+$(function () {
+  smmothScroll(800);
+});
 
 function smmothScroll(duration) {
-	$('a[href^="#"]').on('click', function(e) {
-		var target=$($(this).attr('href'));
-		if(target.length) {
-			e.preventDefault();
-			$('html, body').animate({
-				scrollTop: target.offset().top
-			}, duration);
-		}
-	});
+  $('a[href^="#"]').on("click", function (e) {
+    var target = $($(this).attr("href"));
+    if (target.length) {
+      e.preventDefault();
+      $("html, body").animate(
+        {
+          scrollTop: target.offset().top,
+        },
+        duration
+      );
+    }
+  });
 }
 
-$(window).scroll(function(){
-	superScrollTopButton()
-})
+$(window).scroll(function () {
+  superScrollTopButton();
+});
 
-function superScrollTopButton(){
-	if($(document).scrollTop()>50) {
-		$('#btnTop a').addClass('super-btn-top');
-	}else {
-		$('#btnTop a').removeClass('super-btn-top');
-	}
+function superScrollTopButton() {
+  if ($(document).scrollTop() > 50) {
+    $("#btnTop a").addClass("super-btn-top");
+  } else {
+    $("#btnTop a").removeClass("super-btn-top");
+  }
 }
